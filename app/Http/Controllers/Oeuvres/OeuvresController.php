25c468424097a8699c5194c2a643b223078537e7
Mathieu33260 <mathieu.dumez@etu.u-bordeaux.fr>
2017-10-26 12:05:17 +0200

    controllers creation

---
+++ app/Http/Controllers/Oeuvres/OeuvresController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Oeuvres;
+
+use App\Models\Oeuvre;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OeuvresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Oeuvre  $oeuvre
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Oeuvre $oeuvre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Oeuvre  $oeuvre
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Oeuvre $oeuvre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Oeuvre  $oeuvre
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Oeuvre $oeuvre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Oeuvre  $oeuvre
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Oeuvre $oeuvre)
+    {
+        //
+    }
+}

b2047a7c0b60132be178b0915a815f7d5be02c48
Morgane Julien <morgane.julien@etu.u-bordeaux.fr>
2017-11-05 15:28:35 +0100

    start of the layout home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -72,32 +72,40 @@
 </div>
 </section>
 <!---------------------------------------------------------->
-<section>
+<section id="presentation">
 <div class="flex-center position-ref full-height dark">
 
 
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <h2 class="text-uppercase text-light text-center col-lg-12">Présentation</h2>
-            <p class="text-light col-lg-8 text-center font-weight-bold">_____</p>
-            <p class="text-light col-lg-6 text-center font-weight-bold mr-1 ml-1">Nous sommes une start-up avec une idée originale qui
+            <hr>
+        </div>
+        <div class="row">
+          <div class="col-lg-offset-2 col-lg-8 col-lg-offset-2">
+              <p class="text-light col-lg-12 text-center ">Nous sommes une start-up avec une idée originale qui
                 souhaite révolutionner l'art en apportant une touche de modernité.</p>
-            <p class="text-light col-lg-6 text-center font-weight-bold">En effet, Leonart vous offre une application web capable
-                de faire revivre à vos visiteurs les oeuvres d'arts qu'ils ont déjà découvert, en réalité augmentée.</p>
+              <p class="text-light col-lg-12 text-center ">En effet, Leonart vous offre une application web capable
+                  de faire revivre à vos visiteurs les oeuvres d'arts qu'ils ont déjà découvert, en réalité augmentée.</p>
+          </div>
+        </div>
+            <div class="row">
 
-            <div class="row justify-content-center mt-4">
-
-                <div class="card col-lg-3 mr-4 shadow">
-                    <img class="card-img-top" src="..." alt="Card image cap">
-                    <div class="card-body text-dark font-weight-bold">
+                <div class="col-lg-offset-3 card col-lg-3 shadow">
+                  <div class="row text-center">
+                    <img class="card-img-top" src="{{ asset('/images/Pins_maps.png') }}" alt="Pins map">
+                  </div>
+                    <div class="card-body text-dark">
                         <h4 class="card-title text-center font-weight-bold">Carte iBeacons</h4>
                         <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
 
-                <div class="card col-lg-3 ml-4 shadow">
-                    <img class="card-img-top" src="..." alt="Card image cap">
-                    <div class="card-body text-dark font-weight-bold">
+                <div class="card col-lg-3 shadow">
+                  <div class="row text-center">
+                    <img class="card-img-top" src="{{ asset('/images/MonCompte.png') }}" alt="Mon compte">
+                  </div>
+                    <div class="card-body text-dark">
                         <h4 class="card-title text-center font-weight-bold">Compte</h4>
                         <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
@@ -110,17 +118,14 @@
 </div>
 </section>
 <!---------------------------------------------------------->
-<section>
+<section id="Ibeacon">
 <div class="flex-center position-ref full-height">
-
-
-
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-5 ml-3">
-                <img src="" alt="iBeacon">
+        <div class="row">
+            <div class="col-lg-6">
+                <img src="{{ asset('/images/Ibeacon.png') }}" alt="iBeacon">
             </div>
-            <div class="col-lg-5 mr-3 text-dark font-weight-bold">
+            <div class="col-lg-6">
                 <p>L'iBeacon, est une balise de géolocalisation bluetooth précise, qui permet de détecter
                     et d'intéragir avec un visiteur.<br>
                     Il peut par exemple lui envoyer des informations sur une oeuvre.</p>
@@ -133,11 +138,8 @@
 <!---------------------------------------------------------->
 <section>
 <div class="flex-center position-ref bandeau-height dark">
-
-
-
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <h2 class="text-light text-center col-lg-12 p-4">
                 Prolongez votre visite jusqu'à votre poche !
             </h2>
@@ -146,7 +148,7 @@
 </div>
 </section>
 <!-------------------Phone------------------------->
-<section>
+<section id="phone">
 <div class="flex-center position-ref full-height">
 
 
@@ -160,7 +162,7 @@
                     dans le smartphone.</p>
             </div>
             <div class="col-lg-5 ml-5">
-                <img src="" alt="Carte et smartphone">
+                <img src="{{ asset('/images/phone.png') }}" alt="Carte et smartphone">
             </div>
         </div>
     </div>

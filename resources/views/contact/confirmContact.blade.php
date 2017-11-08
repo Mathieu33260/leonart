fa07e6b637582f85ffbbef7495f65043df366062
Dumez Mathieu <mathieu.dumez@etu.u-bordeaux.fr>
2017-11-08 17:54:06 +0100

    Mail : contact us

---
+++ resources/views/contact/confirmContact.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.app')
+
+@section('title', __("Contact"))
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            <div class="col-xs-12 col-md-8 col-lg-8 panel panel-default">
+
+                <div class="panel-body">
+                    Merci de nous avoir contacté !
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+@endsection
---
+++ resources/views/contact/contact.blade.php
@@ -0,0 +1,35 @@
+@extends('layout.app')
+
+@section('title', __("Contact"))
+
+@section('content')
+    <div class="container">
+        <div class="row">
+
+            <div class="col-xs-12 col-md-8 col-lg-8 panel panel-default">
+                {!! BootForm::open()->action(route('page:contactPost'))->patch() !!}
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            {!! BootForm::text(__("Nom"), 'nom')->required() !!}
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            {!! BootForm::text(__("Email"), 'email')->required() !!}
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            {!! BootForm::text(__("Texte"), 'texte')->required() !!}
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-footer text-right">
+                    <button type="submit" class="btn btn-success">
+                        @lang("Envoyer") <span class="glyphicon glyphicon-ok"></span>
+                    </button>
+                </div>
+                {!! BootForm::close() !!}
+            </div>
+
+
+        </div>
+    </div>
+@endsection

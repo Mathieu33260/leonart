337f520dfc715a2466d81808d7ea18ab58a74c6f
Morgane Julien <morgane.julien@etu.u-bordeaux.fr>
2017-11-14 13:25:48 +0100

    Add bootstrap4

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,27 +4,21 @@
 @section('script')
     @parent
     <script type="application/javascript">
-        function animationBtnContact(hover) {
+        function animationBtn(hover) {
             if(hover)
             {
                 document.getElementById('btnContact').setAttribute(
                     'class','mr-3 p-1 pl-4 pr-4 font-weight-bold text-secondary dark border border-secondary radius nodeco'
                 );
-            } else {
-                document.getElementById('btnContact').setAttribute(
-                    'class','mr-3 p-1 pl-4 pr-4 font-weight-bold text-dark white-grey border-0 radius nodeco'
-                );
-            }
-        }
-        function animationBtnSuivre(hover) {
-            if(hover)
-            {
                 document.getElementById('btnSuivre').setAttribute(
                     'class','ml-3 p-1 pl-4 pr-4 font-weight-bold text-dark white-grey border-0 radius nodeco'
                 );
             } else {
                 document.getElementById('btnSuivre').setAttribute(
                     'class','ml-3 p-1 pl-4 pr-4 font-weight-bold text-secondary dark border border-secondary radius nodeco'
+                );
+                document.getElementById('btnContact').setAttribute(
+                    'class','mr-3 p-1 pl-4 pr-4 font-weight-bold text-dark white-grey border-0 radius nodeco'
                 );
             }
         }
@@ -36,25 +30,8 @@
 
     <div class="top-right links">
 
-
-        @guest
-            <a class="p-3" href="{{ route('login') }}">Login</a>
-            <a class="p-3" href="{{ route('register') }}">Register</a>
-            @else
-                    <a href="{{ route('user:profil:edit') }}"  role="button" aria-expanded="false">
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                            <a href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                Logout
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-                @endguest
+        <a class="p-3" href="{{ route('login') }}">Login</a>
+        <a class="p-3" href="{{ route('register') }}">Register</a>
 
     </div>
 
@@ -72,37 +49,33 @@
 </div>
 </section>
 <!---------------------------------------------------------->
-<section id="presentation">
+<section>
 <div class="flex-center position-ref full-height dark">
 
 
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-center">
             <h2 class="text-uppercase text-light text-center col-lg-12">Présentation</h2>
-            <hr>
-        </div>
-        <div class="row">
-          <div class="col-lg-offset-2 col-lg-8 col-lg-offset-2">
-              <p class="text-light col-lg-12 text-center ">Nous sommes une start-up avec une idée originale qui
+            <p class="text-light col-lg-8 text-center font-weight-bold">_____</p>
+            <p class="text-light col-lg-7 text-center mr-1 ml-1">Nous sommes une start-up avec une idée originale qui
                 souhaite révolutionner l'art en apportant une touche de modernité.</p>
-              <p class="text-light col-lg-12 text-center ">En effet, Leonart vous offre une application web capable
-                  de faire revivre à vos visiteurs les oeuvres d'arts qu'ils ont déjà découvert, en réalité augmentée.</p>
-          </div>
-        </div>
-            <div class="row">
+            <p class="text-light col-lg-7 text-center ">En effet, Leonart vous offre une application web capable
+                de faire revivre à vos visiteurs les oeuvres d'arts qu'ils ont déjà découvert, en réalité augmentée.</p>
 
-                <div class="col-lg-offset-3 card col-lg-3 shadow">
-                  <div class="row text-center">
-                    <img class="card-img-top" src="{{ asset('/images/Pins_maps.png') }}" alt="Pins map">
-                  </div>
+            <div class="row justify-content-center mt-4">
+
+                <div class="card col-lg-3 mr-4 shadow">
+                  <div class="row justify-content-center">
+                     <img class="card-img-top" src="{{ asset('/images/Pins_maps.png') }}" alt="Pins map">
+                   </div>
                     <div class="card-body text-dark">
                         <h4 class="card-title text-center font-weight-bold">Carte iBeacons</h4>
-                        <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text text-center">Découvrer l'ensemble des oeuvres et monuments à voir ou à revoir.</p>
                     </div>
                 </div>
 
-                <div class="card col-lg-3 shadow">
-                  <div class="row text-center">
+                <div class="card col-lg-3 ml-4 shadow">
+                  <div class="row justify-content-center">
                     <img class="card-img-top" src="{{ asset('/images/MonCompte.png') }}" alt="Mon compte">
                   </div>
                     <div class="card-body text-dark">
@@ -118,18 +91,22 @@
 </div>
 </section>
 <!---------------------------------------------------------->
-<section id="Ibeacon">
+<section id="iBeacon">
 <div class="flex-center position-ref full-height">
+
+
+
     <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 ">
                 <img src="{{ asset('/images/Ibeacon.png') }}" alt="iBeacon">
             </div>
-            <div class="col-lg-6">
+            <div class="align-self-center col-lg-6 text-dark ">
                 <p>L'iBeacon, est une balise de géolocalisation bluetooth précise, qui permet de détecter
                     et d'intéragir avec un visiteur.<br>
                     Il peut par exemple lui envoyer des informations sur une oeuvre.</p>
                 <p>La balise iBeacon est discrète est peut se cacher derrière une oeuvre.</p>
+
             </div>
         </div>
     </div>
@@ -138,8 +115,11 @@
 <!---------------------------------------------------------->
 <section>
 <div class="flex-center position-ref bandeau-height dark">
+
+
+
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-center">
             <h2 class="text-light text-center col-lg-12 p-4">
                 Prolongez votre visite jusqu'à votre poche !
             </h2>
@@ -148,11 +128,8 @@
 </div>
 </section>
 <!-------------------Phone------------------------->
-<section id="phone">
+<section>
 <div class="flex-center position-ref full-height">
-
-
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-5 p-3 pr-5 mr-3 text-dark font-weight-bold border-yellow-right">
@@ -162,7 +139,7 @@
                     dans le smartphone.</p>
             </div>
             <div class="col-lg-5 ml-5">
-                <img src="{{ asset('/images/phone.png') }}" alt="Carte et smartphone">
+              <img src="{{ asset('/images/phone.png') }}" alt="Carte et smartphone">
             </div>
         </div>
     </div>
@@ -181,13 +158,13 @@
         </div>
 
         <div class="row justify-content-center">
-            <a id="btnContact" href="{{ route('page:contact') }}" class="mr-3 p-1 pl-4 pr-4 font-weight-bold
-                    text-dark white-grey border-0 radius nodeco" onmouseover="animationBtnContact(true);"
-               onmouseout="animationBtnContact(false)">Contact</a>
+            <a id="btnContact" href="" class="mr-3 p-1 pl-4 pr-4 font-weight-bold
+                    text-dark white-grey border-0 radius nodeco" onmouseover="animationBtn(true);"
+               onmouseout="animationBtn(false)">Contact</a>
 
             <a id="btnSuivre" href="" class="ml-3 p-1 pl-4 pr-4 text-secondary font-weight-bold
-                    dark border border-secondary radius nodeco" onmouseover="animationBtnSuivre(true);"
-               onmouseout="animationBtnSuivre(false)">Suivre</a>
+                    dark border border-secondary radius nodeco" onmouseover="animationBtn(true);"
+               onmouseout="animationBtn(false)">Suivre</a>
 
         </div>
     </div>

0d49cb7436d7da7b620d973a85dbda0642c990e0
Mathieu33260 <mathieu.dumez@etu.u-bordeaux.fr>
2017-11-18 13:56:42 +0100

    Fix some nav problem (front)

--- resources/views/layout/navbarFront.blade.php
+++ resources/views/layout/navbarFront.blade.php
@@ -13,25 +13,21 @@
                 <a class="p-3 font-weight-bold nav-item nav-link" href="{{ route('register') }}">Register</a>
             </div>
             @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                    <div class="navbar-nav links ">
+                        <a class="p-3 font-weight-bold nav-item nav-link" href="{{ route('home') }}">
+                            {{ Auth::user()->name }}
                         </a>
 
-                        <ul class="dropdown-menu" role="menu">
-                            <li>
-                                <a href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
+                        <a class="p-3 font-weight-bold nav-item nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
+                            Logout
+                        </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </div>
             @endguest
         </div>
     </nav>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -44,14 +44,13 @@
                         <a class="p-3 font-weight-bold nav-item nav-link" href="{{ route('register') }}">Register</a>
                     </div>
                     @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+
+                        <div class="navbar-nav links ">
+                            <a class="p-3 font-weight-bold nav-item nav-link" href="{{ route('home') }}">
+                                {{ Auth::user()->name }}
                             </a>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="{{ route('logout') }}"
+                                    <a class="p-3 font-weight-bold nav-item nav-link" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         Logout
@@ -60,9 +59,7 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         {{ csrf_field() }}
                                     </form>
-                                </li>
-                            </ul>
-                        </li>
+                        </div>
                         @endguest
             </div>
         </nav>

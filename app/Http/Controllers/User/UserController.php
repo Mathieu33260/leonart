a8bcbb7d789d9c8764fbe9c503a9aced7aae8473
Mathieu33260 <mathieu.dumez@etu.u-bordeaux.fr>
2017-11-22 21:12:35 +0100

    Safe delete of the second User.php

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller

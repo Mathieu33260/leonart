70944f6033bda610e5d89ab3dc32ee9aa68e44f5
Mathieu33260 <mathieu.dumez@etu.u-bordeaux.fr>
2017-11-22 17:04:27 +0100

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 @section('subtitle', __("Home"))
 
 @section('content')
+
     	<div class="col-md-12 d-flex align-items-center justify-content-md-center" id="banner">
 			<h1 class="display-1 text-white">Bienvenue {{ Auth::user()->name }}</h1>
     	</div>

8360437938293d62c30782bab7741dc395f88944
dubrasquentin <dubrasquentin@gmail.com>
2017-11-22 15:13:29 +0100

    Change page:oeuvre style

--- resources/views/oeuvre/showAjax.blade.php
+++ resources/views/oeuvre/showAjax.blade.php
@@ -1,31 +1,43 @@
 <div class="container">
     <div class="row">
 
-        <div class="col-xs-12 col-md-5 col-lg-5 panel panel-default">
-            <h3>Oeuvre <a href="{{ route('oeuvre:show',['id' => $oeuvre->id]) }}">{{ $oeuvre->nom }}</a></h3>
-            <p>Nom : {{ $oeuvre->nom }}</p>
-            <p>Modèle : {{ $oeuvre->modele }}</p>
-            <p>Id iBeacon : {{ $oeuvre->idIbeacon }}</p>
-            <p>Latitude : {{ $oeuvre->posX }}</p>
-            <p>Longitude : {{ $oeuvre->posY }}</p>
-            <p>Audio : {{ $oeuvre->audio }}</p>
-            @if($oeuvre->type != null)
-                <p>Type :
-                    <a href="{{ route('type:show',['id' => $oeuvre->type->id]) }}">
-                        {{ $oeuvre->type->libelle }}
-                    </a>
-                </p>
-            @endif
-            @if($oeuvre->artiste != null)
-                <p>Artiste :
-                    <a href="{{ route('artiste:show',['id' => $oeuvre->artiste->id]) }}">
-                        {{ $oeuvre->artiste->nom }} {{ $oeuvre->artiste->prenom }}
-                    </a>
-                </p>
-            @endif
-            <p>User : {{ $oeuvre->user->name }}</p>
-            <a href="{{ route('oeuvre:edit', ['id' => $oeuvre->id]) }}">Modifier</a>
-            <a href="{{ route('oeuvre:destroy', ['id' => $oeuvre->id]) }}">Supprimer</a>
+        <div class="col-xs-12 col-md-12 col-lg-12 panel panel-default">
+            <table class="table">
+                <th> <h3>Oeuvre <a href="{{ route('oeuvre:show',['id' => $oeuvre->id]) }}">{{ $oeuvre->nom }}</a></h3></th>
+                <tr>
+                    <td><p>Nom : {{ $oeuvre->nom }}</p></td>
+                    <td>
+                        @if($oeuvre->type != null)
+                            <p>Type :
+                                <a href="{{ route('type:show',['id' => $oeuvre->type->id]) }}">
+                                    {{ $oeuvre->type->libelle }}
+                                </a>
+                            </p>
+                        @endif
+                    </td>
+                    <td>
+                        @if($oeuvre->artiste != null)
+                            <p>Artiste :
+                                <a href="{{ route('artiste:show',['id' => $oeuvre->artiste->id]) }}">
+                                    {{ $oeuvre->artiste->nom }} {{ $oeuvre->artiste->prenom }}
+                                </a>
+                            </p>
+                        @endif
+                    </td>
+                    <td><p>Id iBeacon : {{ $oeuvre->idIbeacon }}</p></td>
+                </tr>
+                <tr>
+                    <td><p>Modèle : {{ $oeuvre->modele }}</p></td>
+                    <td><p>Latitude : {{ $oeuvre->posX }}</p></td>
+                    <td><p>Longitude : {{ $oeuvre->posY }}</p></td>
+                    <td><p>Audio : {{ $oeuvre->audio }}</p></td>
+                </tr>
+                <tr>
+                    <td><p>User : {{ $oeuvre->user->name }}</p></td>
+                    <td><a type="button" class="btn btn-outline-info" href="{{ route('oeuvre:edit', ['id' => $oeuvre->id]) }}">Modifier</a></td>
+                    <td><a type="button" class="btn btn-outline-danger" href="{{ route('oeuvre:destroy', ['id' => $oeuvre->id]) }}">Supprimer</a></td>
+                </tr>
+            </table>
         </div>
 
 

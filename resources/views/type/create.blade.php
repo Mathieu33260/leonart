9c251c5ecd4e284b9855ebb4e9e8847777e4e370
Mathieu33260 <mathieu.dumez@etu.u-bordeaux.fr>
2017-11-29 12:55:09 +0100

    Create type design

--- resources/views/type/create.blade.php
+++ resources/views/type/create.blade.php
@@ -9,28 +9,34 @@
 @endsection
 
 @include('layout.heading')
-<div class="container">
-    <div class="row">
 
-        <div class="col-xs-12 col-md-8 col-lg-8 panel panel-default">
-            {!! Form::open(['route' => 'type:store', 'method' => 'post'])!!}
-            <div class="panel-body">
-                <div class="row">
-                    <div class="col-xs-12 col-md-6">
-                        {!! Form::Label('libelle', 'Libelle') !!}
-                        {!! Form::text('libelle', null, array('required' => 'required', 'class' => 'form-control')) !!}
+<div class="container fluid pt-4">
+    <div class="row justify-content-md-center">
+
+        {!! Form::open(['route' => 'type:store', 'method' => 'post'])!!}
+
+        <div class="row justify-content-md-center">
+            <div class="card">
+                <div class="card-block" style="width: 40rem;">
+                    <div class="row justify-content-md-center">
+                        <div class="col-xs-10 col-md-6 col-lg-6 text-center">
+                            <h6>{!! Form::Label('libelle', 'Libellé du type') !!}</h6>
+                            {!! Form::text('libelle', null, array('required' => 'required', 'class' => 'form-control',
+                            'placeholder' => 'Libellé')) !!}
+                        </div>
                     </div>
+                    <div class="row justify-content-md-center">
+                        <div class="col-xs-12 col-md-6 col-lg-6 text-center mt-4 mb-4">
+                            <button type="submit" class="btn btn-success">
+                                @lang("Sauvegarder") <span class="glyphicon glyphicon-ok"></span>
+                            </button>
+                        </div>
+                    </div>
+                    {!! Form::close() !!}
                 </div>
             </div>
-            <div class="panel-footer text-right">
-                <button type="submit" class="btn btn-success">
-                    @lang("Sauvegarder") <span class="glyphicon glyphicon-ok"></span>
-                </button>
-            </div>
-            {!! Form::close() !!}
         </div>
-
-
     </div>
 </div>
+
 @endsection
